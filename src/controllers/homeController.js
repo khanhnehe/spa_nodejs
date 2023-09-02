9e6bef110a1baf6a28debe7cd659f01f9283aef8
khanhNguyen <nguyenngockhanhvthg@gmail.com>
2023-09-02 14:27:03 +0700

    video21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,18 @@
+import db from "../models/index";
 //luôn truyền 2 tham số là request và response
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs"); //giải thích
+let getHomePage = async (req, res) => {
+  //bắt exception
+  //sau này mỗi lần connect đến db ta sẽ dùng try and catch
+  try {
+    //doc
+    let data = await db.User.findAll();
+
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    }); //doc
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 let getAboutPage = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser"; // thư viện hổ trọ cho we ta có thể lấy đc các tham số mà phía client sd cho we
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
+import connectDB from "./config/connectDB";
+
 require("dotenv").config(); // cái này giúp we chạy dòng này process.env.PORT
 
 let app = express();
@@ -15,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //config app
 viewEngine(app);
 initWebRouters(app);
+
+connectDB();
 
 //bắt buộc có để app chạy, như vậy ta sẽ lấy cái tham số có tên là port ở trong file env
 let port = process.env.PORT || 6969;
